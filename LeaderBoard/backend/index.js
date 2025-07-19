1b66c3ea2628394281a7ef800c7a7f2ccc3dde7b
AnshulVermaQ <146179858+AnshulVermaQ@users.noreply.github.com>
2025-07-19 15:45:01 +0530

    Update index.js

--- LeaderBoard/backend/index.js
+++ LeaderBoard/backend/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 connectDB();
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: 'https://leader-board-frontend-git-main-anshul-vermas-projects-5094b606.vercel.app/', 
+    origin: 'https://localhost:5173/', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 }));
